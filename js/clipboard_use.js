051bcb05a7edaabe71d6bb3c1219ce33c63a26f9
codeape <1845593696@qq.com>
2020-04-05 11:21:42 +0800

    Site updated: 2020-04-05 11:21:32

--- js/clipboard_use.js
+++ js/clipboard_use.js
@@ -16,13 +16,13 @@ $(".highlight").wrap("<div class='code-wrapper' style='position:relative'></div>
         clipboard.on('success', function (e) {
             e.trigger.innerHTML = "<i class='fa fa-check'></i><span>复制成功</span>"
             setTimeout(function () {
-                e.trigger.innerHTML = "<i class='fa fa-exclamation'></i><span>复制</span>"
+                e.trigger.innerHTML = "<i class='fa fa-clipboard'></i><span>复制</span>"
             }, 1000)
            
             e.clearSelection();
         });
         clipboard.on('error', function (e) {
-            e.trigger.innerHTML = "<i class='fa fa-clipboard'></i><span>复制失败</span>"
+            e.trigger.innerHTML = "<i class='fa fa-exclamation'></i><span>复制失败</span>"
             setTimeout(function () {
                 e.trigger.innerHTML = "<i class='fa fa-clipboard'></i><span>复制</span>"
             }, 1000)
